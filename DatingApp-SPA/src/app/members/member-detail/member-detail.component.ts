7d58d7e8049b8c81ebc4229e700dc784b99c8050
Dnyaneshwar Raut <dnyaneshwar.raut@jadeglobal.com>
2018-11-12 15:09:50 +0530

    section 9

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,7 +3,7 @@ import { UserService } from 'src/app/_service/user.service';
 import { AlertifyService } from 'src/app/_service/alertify.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
-import { error } from 'protractor';
+import { NgxGalleryOptions, NgxGalleryAnimation, NgxGalleryImage } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -12,17 +12,39 @@ import { error } from 'protractor';
 })
 export class MemberDetailComponent implements OnInit {
  user: User;
+ galleryOptions: NgxGalleryOptions[];
+ galleryImages: NgxGalleryImage[];
+ 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.loadUser();
-  }
-
-  loadUser() {
-    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
-      this.user = user;
-    }, err => {
-        this.alertify.error(err);
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
     });
+ 
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
+  }
+ 
+  getImages() {
+    const imageUrls = [];
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imageUrls;
   }
 }
